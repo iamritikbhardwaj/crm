dcc7cdb58ebc9765ce3148729377b5b71196ef12
Ritik Singh <ritiklrt2@gmail.com>
2025-02-26 17:50:33 +0530

    minor tweeking an logo changes

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,6 +1,9 @@
 import React, { use, useEffect, useState } from "react";
 import BackToHome from "../components/BackToHome";
-import { CustomTable, DefaultColumnFilter } from "../components/customTable/CustomTable";
+import {
+  CustomTable,
+  DefaultColumnFilter,
+} from "../components/customTable/CustomTable";
 import { useNavigate } from "react-router-dom";
 import { GrFormView } from "react-icons/gr";
 import { getTravelMonthRange } from "./Booking";
@@ -23,125 +26,132 @@ const AllBookings = () => {
   const [fromDate, setFromDate] = useState(Date.now()); // this form data will be used to submit all the data required for trip creation
   const [toDate, setToDate] = useState(Date.now() + new Date(86400000)); // filter related data
 
-  const search = async() => { // this function is for fetching trip data as per the date range
+  const search = async () => {
+    // this function is for fetching trip data as per the date range
     await fetchTrips();
     const data = await fetchFilteredTrips(fromDate, toDate);
-      const bookings = data ? 
-      data.map(
-        (item) =>
-          (user &&
-            user.profile === "Sales" &&
-            user.name !== item.salesSpoc) || {
-              arrivalDate:
-              item.arrivalDate,
-            departureDate:
-              item.departureDate,
-            tripID: item.tripId,
-            destination: item.destination,
-            bookingDate:
-              item.bookingDate.slice(8, 10) +
-              "/" +
-              item.bookingDate.slice(5, 7) +
-              "/" +
-              item.bookingDate.slice(0, 4),
-            customerName: (
-              <div className="leading-[0.7]">
-                <p>{item.customerName}</p> <br />
-                <p>
-                  {item.pax?.A} A / {item.pax?.C} C
-                </p>
-              </div>
-            ),
-            salesSPOC: item.salesSpoc,
-            agent: item.agent,
-            travelMonth: getTravelMonthRange(
-              item.arrivalDate,
-              item.departureDate
-            ),
-            contactDetails: `${item.countryCode} / ${item.whatsappNumber}`,
-            transferPrice: user.profile === "Sales" ? 'N/A' : (parseFloat(item?.transferPrice) || 0) + " USD",
-            orderValue: item.orderValue + " USD",
-            apayment: (
-              <div
-                className={`font-bold ${
-                  parseFloat(item.orderValue) === parseFloat(item?.payment)
-                    ? "text-green-500"
-                    : "text-red-500"
-                }`}
-              >
-                {item?.payment + " USD"}
-              </div>
-            ),
-            status: (
-              <button
-                className={`${
-                  (item.status === "CANCELLED" && "bg-red-500") ||
-                  (item.status === "ON-TOUR" && "bg-yellow-500") ||
-                  (item.status === "CONFIRMED" && "bg-green-500") ||
-                  (item.status === "TRAVELLED" && "bg-gray-500")
-                } ${
-                  item.status === "IN-PROGRESS" ? "bg-blue-500" : ""
-                } text-white p-1 m-1 rounded-full`}
-              >
-                {!item.status ? "CONFIRMED" : item.status}
-              </button>
-            ),
-            opsSpoc: item.opsSpoc,
-            action: (
-              <button
-                className="text-3xl"
-                onClick={() => navigate("/viewAllBooking", { state: item })}
-              >
-                <GrFormView />
-              </button>
-            ),
-            paymentstat: (
-              <button
-                className={`${
-                  item.paymentStatus === "FULL-PAID" ? "bg-green-400" : "bg-red-400"
-                } p-2 rounded-lg`}
-              ></button>
-            ),
-            validation: (
-              <button
-                className={`${
-                  item.validation === "Finance" && "bg-green-400"
-                } ${
-                  item.validation === "Operations"
-                    && "bg-blue-400"
-                } ${
-                  item.validation === "Finance" || item.validation === "Operations" || "bg-red-400"
-                    && "bg-red-400"
-                }
+    const bookings = data
+      ? data
+          .map(
+            (item) =>
+              (user &&
+                user.profile === "Sales" &&
+                user.name !== item.salesSpoc) || {
+                arrivalDate: item.arrivalDate,
+                departureDate: item.departureDate,
+                tripID: item.tripId,
+                destination: item.destination,
+                bookingDate:
+                  item.bookingDate.slice(8, 10) +
+                  "/" +
+                  item.bookingDate.slice(5, 7) +
+                  "/" +
+                  item.bookingDate.slice(0, 4),
+                customerName: (
+                  <div className="leading-[0.7]">
+                    <p>{item.customerName}</p> <br />
+                    <p>
+                      {item.pax?.A} A / {item.pax?.C} C
+                    </p>
+                  </div>
+                ),
+                salesSPOC: item.salesSpoc,
+                agent: item.agent,
+                travelMonth: getTravelMonthRange(
+                  item.arrivalDate,
+                  item.departureDate
+                ),
+                contactDetails: `${item.countryCode} / ${item.whatsappNumber}`,
+                transferPrice:
+                  user.profile === "Sales"
+                    ? "N/A"
+                    : (parseFloat(item?.transferPrice) || 0) + " USD",
+                orderValue: item.orderValue + " USD",
+                apayment: (
+                  <div
+                    className={`font-bold ${
+                      parseFloat(item.orderValue) === parseFloat(item?.payment)
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }`}
+                  >
+                    {item?.payment + " USD"}
+                  </div>
+                ),
+                status: (
+                  <button
+                    className={`${
+                      (item.status === "CANCELLED" && "bg-red-500") ||
+                      (item.status === "ON-TOUR" && "bg-yellow-500") ||
+                      (item.status === "CONFIRMED" && "bg-green-500") ||
+                      (item.status === "TRAVELLED" && "bg-gray-500")
+                    } ${
+                      item.status === "IN-PROGRESS" ? "bg-blue-500" : ""
+                    } text-white p-1 m-1 rounded-full`}
+                  >
+                    {!item.status ? "CONFIRMED" : item.status}
+                  </button>
+                ),
+                opsSpoc: item.opsSpoc,
+                action: (
+                  <button
+                    className="text-3xl"
+                    onClick={() => navigate("/viewAllBooking", { state: item })}
+                  >
+                    <GrFormView />
+                  </button>
+                ),
+                paymentstat: (
+                  <button
+                    className={`${
+                      item.paymentStatus === "FULL-PAID"
+                        ? "bg-green-400"
+                        : "bg-red-400"
+                    } p-2 rounded-lg`}
+                  ></button>
+                ),
+                validation: (
+                  <button
+                    className={`${
+                      item.validation === "Finance" && "bg-green-400"
+                    } ${item.validation === "Operations" && "bg-blue-400"} ${
+                      item.validation === "Finance" ||
+                      item.validation === "Operations" ||
+                      ("bg-red-400" && "bg-red-400")
+                    }
          p-2 rounded-lg`}
-              ></button>
-            ),
-            opsstatus: (
-              <button
-                className={`${
-                  item.opsStatus === "COMPLETED" ? "bg-green-400" : "bg-red-400"
-                } p-2 rounded-lg`}
-              ></button>
-            ),
-          }
-      ).filter((stuff) => stuff !== true) : [];
-      console.log(bookings, "bookings");
-      setBookings(bookings);
-  }
+                  ></button>
+                ),
+                opsstatus: (
+                  <button
+                    className={`${
+                      item.opsStatus === "COMPLETED"
+                        ? "bg-green-400"
+                        : "bg-red-400"
+                    } p-2 rounded-lg`}
+                  ></button>
+                ),
+              }
+          )
+          .filter((stuff) => stuff !== true)
+      : [];
+    console.log(bookings, "bookings");
+    setBookings(bookings);
+  };
 
   useEffect(() => {
     search();
   }, []);
 
   const col = [
-    { Header: "Arrival Dates", accessor: "arrivalDate", Filter: DefaultColumnFilter },
+    { Header: "Arrival Dates", accessor: "arrivalDate" },
     { Header: "Departure Dates", accessor: "departureDate" },
     { Header: "Trip ID", accessor: "tripID" },
     { Header: "Destination", accessor: "destination" },
     { Header: "Booking Date", accessor: "bookingDate" },
     { Header: "Sales SPOC", accessor: "salesSPOC" },
     { Header: "Agent", accessor: "agent" },
-    { Header: "Customer Name", accessor: "customerName", Filter: DefaultColumnFilter  },
+    { Header: "Customer Name", accessor: "customerName" },
     { Header: "Travel Month", accessor: "travelMonth" },
     { Header: "Transfer Price", accessor: "transferPrice" },
     { Header: "Contact Details", accessor: "contactDetails" },
@@ -179,7 +189,11 @@ const AllBookings = () => {
           />
         </div>
         <div>
-          <button onClick={search} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8" type="button">
+          <button
+            onClick={search}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8"
+            type="button"
+          >
             Filter
           </button>
         </div>
